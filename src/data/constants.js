841d9dd4b78209aea9bdd61e7e84de5a74436077
Akash Devaraj <Akash.Devaraj@tecnotree.com>
2024-02-26 01:53:30 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,243 @@
+export const Bio = {
+  name: "Akash H D",
+  roles: [
+    "Web Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Akash8Gowda18",
+  resume:"https://drive.google.com/file/d/1rmxslsHLL7Lc1ciQ6UuIUU_KNERSOI5g/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/akash-h-d-139471167?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B9ZuyPuSuTPuWQ14IGw1mvg%3D%3D",
+ 
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+ 
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      
+      
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      // {
+      //   name: "Python",
+      //   image:
+      //     "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      // },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+  
+    ],
+  },
+ 
+  {
+    title: "Others",
+    skills: [
+      
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+    
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+     
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+      
+      {
+        name: "Software Testing ",
+        image:
+          "",
+      },
+    
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "",
+    role: "Front End Developer-Intern",
+    company: "Was Tecno Developer",
+    date: "May 2023 - Sep 2023",
+    desc: "Worked on company website, UPSC study material e-com website using ReactJs.",
+    skills: [
+      "ReactJs",
+      "CSS",
+      "Bootstrap",
+      "Mysql",
+          ],
+  
+  },
+
+  {
+    id: 1,
+    img: "",
+    role: "Consultant Service Desk",
+    company: "Tenotree",
+    date: "Sep 2023",
+    desc: "Working on Network operation center. Monitoring the OpCos Applications, Billings, company project dashboard etc .",
+    skills: [
+      "PHP",
+      "Mysql",
+          ],
+  
+  },
+ 
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "",
+    school: "The Oxford College of Engineering",
+    date: "Jan 2020 - Sep 2022",
+    grade: "78.37%",
+    desc: "I completed my Master of Computer Application from The Oxford college of Engineering, Banglore. I have taken courses in  Object-Oriented Programming, Database Management Systems, Software testing among others.",
+    degree: "Master of computer Application",
+  },
+  {
+    id: 1,
+    img: "",
+    school: "VidyaAshram First Grade college ",
+    date: "Aug 2017 -  Sep 2019",
+    grade: "64.00%",
+    desc: "I completed my Bachelor of Computer Application from VidyaAshram First Grade College, Mysuru.",
+    degree: "Bachelor of Computer Application ",
+  },
+  {
+    id: 2,
+    img: "",
+    school: "SDM PU College",
+    date: "Jun 2015 - Mar 2019",
+    grade: "67.33%",
+    desc: "I completed my class 12 education at SDM PU College, Ujire .",
+    degree: "Pre University College, PCMB",
+  },
+
+  {
+    id: 3,
+    img: "",
+    school: "S Manasa High School",
+    date: "Mar 2015",
+    grade: "81.28%",
+    desc: "I completed my class 10 education at S Manasa High School, Aldur.",
+    degree: "SSLC",
+  },
+];
+
+export const projects = [
+  {
+    id: 4,
+    title: "Simple Portfolio",
+    date: "May 2023 - May 2023",
+    description:
+      "A simple portfolio created using HTML/CSS and Bootstrap ",
+    image:
+    "https://github.com/Akash8Gowda18/Portfolio/blob/main/css/image/simple%20Portfolio.png?raw=true",
+    tags: ["HTML", "CSS", "Bootstrap"],
+    category: "Web app",
+    github: "https://github.com/Akash8Gowda18/Portfolio/tree/main/css",
+    webapp: "https://akash-portfolio-bootstrap.netlify.app/",
+  },
+
+  {
+    id: 5,
+    title: "Gpt-3",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "The GPT-3 application represents a significant step forward in making advanced natural language processing accessible to a broad audience. By leveraging the power of React for the front end, the application provides a robust, efficient, and engaging platform for users to explore the capabilities of AI-driven language models.",
+    image:
+      "https://github.com/Akash8Gowda18/react-gpt/blob/master/public/gpt-3.png?raw=true",
+    tags: [
+      
+      "React Js",
+      "CSS",
+    ],
+    category: "web app",
+    github: "https://github.com/Akash8Gowda18/react-gpt",
+    webapp: "https://akash-react-gpt.netlify.app/",
+  },
+  
+  {
+    id: 6,
+    title: "UPSC Study Materials UI",
+    date: "May 2023 - Aug 2023",
+    description:
+      "I developed the front-end of an e-commerce website named 'UPSC Study Material,' utilizing React.js and CSS for a seamless user experience. This responsive application serves as a comprehensive platform for competitive exam resources, offering a wide range of books and stationery items tailored to UPSC aspirants' needs.",
+    image:
+      "https://github.com/Akash8Gowda18/UPSC-UI/blob/main/public/UPSC%20Studymaterial.png?raw=true",
+    tags: ["React Js", "CSS"],
+    category: "web app",
+    github: "https://github.com/Akash8Gowda18/UPSC-UI.git",
+    webapp: "https://upcsui.netlify.app",
+  },
+
+  {
+    id: 6,
+    title: "Portfolio",
+    description:
+      "I crafted a dynamic and visually engaging online portfolio, showcasing my diverse project experiences, including web development and design work. This portfolio not only highlights my technical proficiency in modern web technologies but also demonstrates my creative approach to solving user experience challenges across various digital platforms.",
+    image:
+      "",
+    tags: ["React Js", "CSS"],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+  
+];
+
+
